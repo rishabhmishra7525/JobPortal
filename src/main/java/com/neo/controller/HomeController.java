3a04cd33752152bd9aec3787fb04fb97e0b3517f
rishabhmishra7525 <92629493+rishabhmishra7525@users.noreply.github.com>
2021-12-04 17:15:49 +0530

    nit-0002

--- src/main/java/com/neo/controller/HomeController.java
+++ src/main/java/com/neo/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.neo.controller;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -12,4 +15,9 @@ public class HomeController {
       {
 		return "home.html";
 		}
+	@PostMapping("/")
+	public String showHomePage()
+	{
+		return "home.html";
+	}
 }
